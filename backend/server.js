3d028abd302a8099984a0cd5b3f7edf471bec658
ALANxBENNY <alan44benny@gmail.com>
2025-03-07 09:50:49 +0530

    Removed deprecated MongoDB options

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json()); // Parse JSON request bodies
 // MongoDB Connection
 const MONGO_URI = process.env.MONGO_URI;
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
